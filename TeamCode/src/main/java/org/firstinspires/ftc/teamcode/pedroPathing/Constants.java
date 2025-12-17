3f7a8950dd3ba82766972c9b102dcc27ea20a27c
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2025-12-17 11:58:28 -0500

    finished on minute 28:21 out of 40

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,7 +15,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(10); // 10 kg is the estimate; NEED TO ACTUALLY WEIGH IT
+            .mass(10) // 10 kg is the estimate; NEED TO ACTUALLY WEIGH IT
+            .forwardZeroPowerAcceleration(-27.489503988651062)
+            .lateralZeroPowerAcceleration(-93.56352600735903)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.14,0, 0.02, 0.04))
+            .headingPIDFCoefficients(new PIDFCoefficients(1,0,0.02,0))
+            ;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
@@ -25,7 +31,11 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorEx.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorEx.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorEx.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD)
+            .xVelocity(71.36550494817297)
+            .yVelocity(59.67274823526698);
+
+
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(0.0) //forward and back pod is exactly aligned in the middle
