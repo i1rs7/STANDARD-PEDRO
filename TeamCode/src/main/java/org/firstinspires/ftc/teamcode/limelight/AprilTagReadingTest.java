30f3a3b14d0337c25cd529a27a5e2d990f2e6d52
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2026-02-05 11:05:40 -0500

    null stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/AprilTagReadingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight/AprilTagReadingTest.java
@@ -1,7 +1,76 @@
 package org.firstinspires.ftc.teamcode.limelight;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-public class AprilTagReadingTest {
+
+@TeleOp
+public class AprilTagReadingTest extends OpMode {
+
+    private Limelight3A limelight;
+    private double tagId;
+    private IMU imu;
+    //private double distance;
+
+    @Override
+    public void init(){
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(  1); //obelisk = 0, goal = 1
+        imu = hardwareMap.get(IMU.class,"pinpoint");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD); //don't know if the forward direction is right
+        imu.initialize (new IMU.Parameters(revHubOrientationOnRobot));
 
 
+
+    }
+    @Override
+    public void start(){
+        limelight.start();
+
+    }
+    @Override
+    public void loop(){
+
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+
+        if(llResult !=null && llResult.isValid()) {
+            Pose3D botPose = llResult.getBotpose();
+            telemetry.addData("Target X", llResult.getTx());
+            telemetry.addData("Target Y", llResult.getTy());
+            telemetry.addData("Target Area ", llResult.getTa());
+        }
+
+        //limelight.updateRobotOrientation(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+
+
+        //if (llResult != null && llResult.isValid()){
+        //    Pose3D botpose = llResult.getBotpose();
+        //    double camX  = -botpose.getPosition().x;
+        //    double camY  = botpose.getPosition().y;
+        //    distance = getDistance(camX, camY);
+        //    telemetry.addData("distance", distance);
+        //    telemetry.addData("Cam X", camX);
+        //    telemetry.addData("Cam Y", camY);
+        //    telemetry.addData("Target X", llResult.getTx());
+        //    telemetry.addData("Target Area", llResult.getTa());
+        //    telemetry.addData("Botpose", botpose.toString());
+        //} else {
+        //    telemetry.addLine("No result");
+        //}
 
 }
+
+
+    }
