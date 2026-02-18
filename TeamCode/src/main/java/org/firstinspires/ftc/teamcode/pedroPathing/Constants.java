182e4fdcd7f4c4b1a7d7382b6d62fd5c271b75d6
c29st <c29st@dalton.org>
2026-02-18 17:55:53 -0500

    changed offsets yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -39,8 +39,8 @@ public class Constants {
 
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(0.0) //forward and back pod is exactly aligned in the middle THIS IS THE X POD OFFSET
-            .strafePodX(1.0) // left and right pod is 1 inch left of the center THIS IS THE Y POD OFFSET
+            .forwardPodY(0.5) //forward and back pod is exactly aligned in the middle THIS IS THE X POD OFFSET
+            .strafePodX(2.25) // left and right pod is 1 inch left of the center THIS IS THE Y POD OFFSET
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
