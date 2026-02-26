f813cb01e447c7008cde7e5191c316e7dbacba3d
c29st <c29st@dalton.org>
2026-02-26 11:47:46 -0500

    changed pidf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/FlywheelLogic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/FlywheelLogic.java
@@ -66,7 +66,7 @@ public class FlywheelLogic {
         outtakeLeft.setDirection(DcMotor.Direction.REVERSE);
         outtakeRight.setDirection(DcMotor.Direction.FORWARD);
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(15,0,0,13);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(36,0,0,19);
         outtakeLeft.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER,pidfCoefficients);
         outtakeRight.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER,pidfCoefficients);
 
