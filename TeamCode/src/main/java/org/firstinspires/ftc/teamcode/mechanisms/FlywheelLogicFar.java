2686c00379916c0a3adb6473957457d23307c988
c29st <c29st@dalton.org>
2026-03-06 17:59:19 -0500

    autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/FlywheelLogicFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/FlywheelLogicFar.java
@@ -30,7 +30,7 @@ public class FlywheelLogicFar {
     // gate constants
     private double GATE_DOWN_ANGLE = 0.30;
     private double GATE_UP_ANGLE = 0.60;
-    private double GATE_OPEN_TIME = 0.5; // how long we are shooting for
+    private double GATE_OPEN_TIME = 0.25; // how long we are shooting for
     private double GATE_CLOSE_TIME = 0.5;
 
     // ------ SHOOTER CONSTANTS --------
